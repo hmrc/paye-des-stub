15ced2cf473d9c462904f61c2b7a28c7ed15fc32
PabloJSartirana <243117200+PabloJSartirana@users.noreply.github.com>
2026-06-29 16:49:37 +0100

    DLSN-698: Update apiDefinitionJson in spec

--- it/test/PlatformIntegrationSpec.scala
+++ it/test/PlatformIntegrationSpec.scala
@@ -58,6 +58,10 @@ class PlatformIntegrationSpec extends AnyWordSpecLike with Matchers with GuiceOn
           |              "version": "2.0",
           |              "status": "ALPHA",
           |              "endpointsEnabled": false
+          |            },
+          |            { "version": "2.1",
+          |              "status": "ALPHA",
+          |              "endpointsEnabled": false
           |            }
           |        ]
           |    }
